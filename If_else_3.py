681ce4a4a16699fa43ef80836b6af3584ef28aaf
Mark <rasskazovmark77@gmail.com>
2026-05-26 19:35:09 +0500

    Add files via upload

---
+++ If_else_3.py
@@ -0,0 +1,41 @@
+# Задача 1
+# number = int(input("Введи число: "))
+# if number % 2 == 0:
+#     print("Чётное")
+# else:
+#     print("Нечётное")
+
+
+
+
+# Задача 2
+# num1 = int(input("Введите первое число: "))
+# num2 = int(input("Введите второе число: "))
+# oper = input("Операция (+, -, *, /): ")
+# if oper == "+":
+#     print(num1 + num2)
+# elif oper == "-":
+#     print(num1 - num2)
+# elif oper == "*":
+#     print(num1 * num2)
+# elif oper == "/":
+#     print(num1 / num2)
+# else:
+#     print("Неизвестная операция")
+
+
+
+
+
+#Задача 3
+# hour = int(input("Введите час (0-23): "))
+# if 6 <= hour <= 11:
+#     print("Сейчас утро")
+# elif 12 <= hour <= 17:
+#     print("Сейчас день")
+# elif 18 <= hour <= 23:
+#     print("Сейчас вечер")
+# elif 0<= hour <= 5:
+#     print("Сейчас ночь")
+# else:
+#     print("НЕВОЗМОЖНО")
