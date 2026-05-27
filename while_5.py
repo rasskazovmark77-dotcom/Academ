e4f815ef68e5051dbd05fb03d6d5586dcf7f6136
Mark <rasskazovmark77@gmail.com>
2026-05-27 13:13:26 +0500

    Add files via upload

---
+++ while_5.py
@@ -0,0 +1,19 @@
+#Задача 1
+# i = 0
+# while i < 5 :
+#     i = i + 1
+#     print(i)
+
+
+#Задача 2
+# num = 7
+# attempts = 0
+# ans = 0
+# while ans != num:
+#     ans = int(input("Угадайте число от 1 до 10: "))
+#     if ans < num:
+#         print("Больше")
+#     elif ans > num:
+#         print("Меньше")
+#     attempts = attempts + 1
+# print(f"Молодец, тебе понадобилось {attempts} попыток")
