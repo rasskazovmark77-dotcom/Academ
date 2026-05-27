498ad9d46c2d0b73e8bbcc0a4998eb76ac3cb22d
Mark <rasskazovmark77@gmail.com>
2026-05-27 13:13:01 +0500

    Add files via upload

---
+++ for_4.py
@@ -0,0 +1,12 @@
+#Задача 1
+# i = 0
+# for i in range (1, 11):
+#     print("5 x", i, "=", 5 * i)
+
+
+#Задача 2
+# n = int(input("Введите число N: "))
+# total = 0
+# for i in range(1, n + 1):
+#     total = total + i
+# print(f"Сумма чисел от 1 до {n} = {total}")
